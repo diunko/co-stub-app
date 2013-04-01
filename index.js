4769be93b0929cad619063a548334fc6b7866fdc
Dmitry Unkovsky <oil.crayons@gmail.com>
2013-04-01 14:46:12 +0300

    fly!

---
+++ index.js
@@ -0,0 +1,30 @@
+
+// index.js
+var http = require("http")
+var mt = require("microtime")
+
+var S = new http.Server(function(req,res){
+  var body = []
+  var len = 0
+  req.on("data",function(data){
+    body.push(data)
+    len += data.length
+  })
+  req.on("end",function(){
+    res.writeHead(200,{
+      "x-by":"simple node app",
+      "content-type":"text/plain"
+    })
+    res.write("at "+mt.now()+"\n")
+    res.end("on "+req.url+" received body of "+len+" bytes\n")
+  })
+})
+
+if(process.__cocaine){
+  S.listen({_handle:process.__cocaine})
+} else {
+  S.listen(8080)
+}
+
+setTimeout(function(){S.close()},20000)
+
